a63c6de3bdd29919b8ed8fe8c2bd3f9c47158fb5
kotoshka2 <138360810+kotoshka2@users.noreply.github.com>
2025-05-09 13:07:48 +0200

    add mic

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using Game;
 using UnityEngine;
+using Random = UnityEngine.Random;
+
 
 namespace Player
 {
@@ -34,15 +37,48 @@ namespace Player
         private Vector2 moveInput;
         private bool jumpRequested;
 
+        [Header("Audio")] 
+        private AudioSource[] AudioSources;
+        private AudioSource stepMaker;
+        private AudioSource voiceMaker;
+        [SerializeField] private List<AudioClip> concreteSteps;
+        [SerializeField] private List<AudioClip> grassSteps;
+        private List<AudioClip> usedSteps;
+        [SerializeField] private float stepInterval = 0.5f;
+        private float stepTimer = 0f;
+        bool wasGrounded;
+        
+        [Header("Microphone")]
+        public string microphoneDevice;
+        public int recordingLength = 10; // в секундах
+        private AudioClip recordedClip;
+        private bool isRecording = false;
+        private float timer;
+ 
+
         void Start()
         {
             controller = GetComponent<CharacterController>();
+            AudioSources = GetComponents<AudioSource>();
+            stepMaker = AudioSources[0];
+            voiceMaker = AudioSources[1];
             Cursor.lockState = CursorLockMode.Locked;
-            
+            usedSteps = concreteSteps;
+            if (Microphone.devices.Length > 0)
+            {
+                microphoneDevice = Microphone.devices[0];
+            }
+            else
+            {
+                Debug.LogWarning("Микрофон не найден.");
+            }
+
         }
 
         void Update()
         {
+            
+            stepTimer -= Time.deltaTime;
             // Чтение ввода
             moveInput.x = Input.GetAxis("Horizontal");
             moveInput.y = Input.GetAxis("Vertical");
@@ -52,6 +88,21 @@ namespace Player
                 jumpRequested = true;
             }
 
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                timer = Time.time + recordingLength;
+                StartRecording();
+            }
+
+            if (Time.time > timer && isRecording)
+            {
+                StopRecording();
+            }
+
+            if (Input.GetKeyDown(KeyCode.V) && !isRecording)
+            {
+                PlayRecordedAudio();
+            }
             // Сглаживание мыши
             Vector2 targetMouseDelta = new Vector2(
                 Input.GetAxisRaw("Mouse X"),
@@ -82,8 +133,15 @@ namespace Player
                 velocity.y = 0f;
             }
 
+            
+           
+            ChooseTheSound();
             MoveCharacter();
-
+            if (!wasGrounded && isGrounded)
+            {
+                PlayRandomFootstep();
+            }
+            wasGrounded = isGrounded;
             if (jumpRequested && isGrounded)
             {
                 velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
@@ -103,8 +161,14 @@ namespace Player
                 if (move != Vector3.zero)
                 {
                     MakeSound(SoundType.Quiet, transform.position);
+                    if (stepTimer <= 0)
+                    {
+                        PlayRandomFootstep();
+                        stepTimer = stepInterval;
+                    }
                 }
                 controller.Move(move * speed /2 * Time.fixedDeltaTime);
+                
             }
             else
             {
@@ -113,6 +177,11 @@ namespace Player
                     if (move != Vector3.zero)
                     {
                         MakeSound(SoundType.Loud, transform.position);
+                        if (stepTimer <= 0)
+                        {
+                            PlayRandomFootstep();
+                            stepTimer = stepInterval;
+                        }
                     }
                     controller.Move(move * speed * 1.5f * Time.fixedDeltaTime);
                 }
@@ -121,6 +190,11 @@ namespace Player
                     if (move != Vector3.zero)
                     {
                         MakeSound(SoundType.Normal, transform.position);
+                        if (stepTimer <= 0)
+                        {
+                            PlayRandomFootstep();
+                            stepTimer = stepInterval;
+                        }
                     }
                     controller.Move(move * speed * Time.fixedDeltaTime);
                 }
@@ -131,6 +205,79 @@ namespace Player
         void MakeSound(SoundType type, Vector3 position)
         {
             GameManager.SendPlayerMadeSound(type,position);
+        }
+
+        private SurfaceType CheckSurface()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, 3f)) {
+                string surfaceTag = hit.collider.tag;
+
+                switch (surfaceTag) {
+                    case "Grass":
+                        return SurfaceType.Grass;
+                        break;
+                    case "Concrete":
+                        return SurfaceType.Concrete;
+                    default:
+                        return SurfaceType.Concrete;
+                }
+            }
+
+            return SurfaceType.Concrete;
+        }
+
+        private void ChooseTheSound()
+        {
+            switch (CheckSurface())
+            {
+                case SurfaceType.Grass:
+                    usedSteps = grassSteps;
+                    break;
+                case SurfaceType.Concrete:
+                    usedSteps = concreteSteps;
+                    break;
+                default:
+                    usedSteps = concreteSteps;
+                    break;
+            }
+            
+        }
+        private void PlayRandomFootstep()
+        {
+            if (usedSteps.Count == 0 || stepMaker == null || !isGrounded)
+                return;
+            float randPitch = Random.Range(0.8f, 1.2f);
+            stepMaker.pitch = randPitch;
+            int index = Random.Range(0, usedSteps.Count);
+            stepMaker.PlayOneShot(usedSteps[index]);
+        }
+        public void StartRecording()
+        {
+            if (microphoneDevice == null || isRecording) return;
+
+            Debug.Log("Начало записи...");
+            recordedClip = Microphone.Start(microphoneDevice, false, recordingLength, 44100);
+            isRecording = true;
+            
+        }
+
+        public void StopRecording()
+        {
+            if (!isRecording) return;
+
+            Microphone.End(microphoneDevice);
+            isRecording = false;
+            Debug.Log("Запись остановлена.");
+        }
+
+        public void PlayRecordedAudio()
+        {
+            if (recordedClip == null) return;
+
+            voiceMaker.clip = recordedClip;
+            voiceMaker.Play();
+            Debug.Log("Воспроизведение записи...");
         }
     }
 }
